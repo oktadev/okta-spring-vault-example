45dd8f8a9a36aa080ddd8e9f6aa1e2f3e82af174
Matt Raible <matt.raible@okta.com>
2020-04-21 15:53:31 -0600

    Updates after QA

--- vault-config-server/src/main/java/com/okta/developer/vault/SpringBootConfigurationServerApplication.java
+++ vault-config-server/src/main/java/com/okta/developer/vault/SpringBootConfigurationServerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class SpringBootConfigurationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootConfigurationServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigurationServerApplication.class, args);
+    }
 
 }
--- vault-demo-app/src/main/java/com/okta/developer/vault/Application.java
+++ vault-demo-app/src/main/java/com/okta/developer/vault/Application.java
@@ -7,17 +7,16 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 	@GetMapping("/")
-	String hello(@AuthenticationPrincipal OidcUser user){
+	String hello(@AuthenticationPrincipal OidcUser user) {
 		return String.format("Welcome, %s", user.getFullName());
 	}
 
